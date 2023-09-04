228126e11f5e0a6433f45b70460550f2c5f8bb8d
PriyaSingh03 <singhpriyaangle@gmail.com>
2023-09-04 09:03:54 +0530

    final changes

--- app_1/FIR.py
+++ app_1/FIR.py
@@ -1,12 +1,12 @@
 import streamlit as st
-import os
 import pandas as pd
 import folium
 from folium.plugins import HeatMap
 from streamlit_folium import st_folium
 
-current_directory = os.getcwd()
-st.write(f"Current working directory: {current_directory}")
+# current_directory = os.getcwd()
+# st.write(f"Current working directory: {current_directory}")
+
 
 @st.cache_data  # Cache the data to improve performance
 def load_data():
@@ -14,6 +14,7 @@ def load_data():
     st.write(f"Loading data from: {current_directory}")
     data = pd.read_csv('PERFECT_FIR_DATASET.csv')
     return data
+
 
 def get_color_based_on_parameter(row, parameter):
     # Define a colormap based on the selected parameter
@@ -26,16 +27,19 @@ def get_color_based_on_parameter(row, parameter):
     parameter_value = row[parameter]
 
     # Map the parameter value to a color from the colormap
-    color = colormap.get(parameter, 'red')  # Default to 'blue' if parameter is not found in the colormap
+    # Default to 'blue' if parameter is not found in the colormap
+    color = colormap.get(parameter, 'red')
     return color
+
 
 def get_color_based_on_time(row):
     # Extract the hour component from the 'DateTime' column
-    datetime_value = pd.to_datetime(row['DateTime'], format='%Y-%m-%d %H:%M:%S')
+    datetime_value = pd.to_datetime(
+        row['DateTime'], format='%Y-%m-%d %H:%M:%S')
     hour = datetime_value.hour
 
     # Check if the hour is before 15:00 (3 pm)
-    if (hour <15):
+    if (hour < 15):
         return 'lightgreen'
     else:
         return 'cadetblue'
@@ -50,8 +54,8 @@ def app():
     # Rename the 'Latitude' and 'Longitude' columns to 'lat' and 'lon' (standard for Streamlit)
     data.rename(columns={'Latitude': 'lat', 'Longitude': 'lon'}, inplace=True)
 
-    
-    selected_parameter = st.sidebar.radio("Select parameter for visualization", ['Markers', 'Heatmap'])
+    selected_parameter = st.sidebar.radio(
+        "Select parameter for visualization", ['Markers', 'Heatmap'])
 
     # # Create a Streamlit map
     # st.map(data, use_container_width=True)
@@ -60,13 +64,13 @@ def app():
     min_longitude = data['lon'].min()
     max_longitude = data['lon'].max()
 
+    UP_AREA = [(min_latitude + max_latitude) / 2,
+               (min_longitude + max_longitude) / 2]
+    map = folium.Map(location=UP_AREA, zoom_start=6)  # tiles='Stamen Toner'
 
-    UP_AREA = [(min_latitude + max_latitude) / 2, (min_longitude + max_longitude) / 2]
-    map  = folium.Map(location=UP_AREA, zoom_start=6) #tiles='Stamen Toner'
-
-    
     if selected_parameter == 'Markers':
-        selected_mapping_parameter = st.sidebar.radio("Select parameter for color mapping", [None, 'DateTime'])
+        selected_mapping_parameter = st.sidebar.radio(
+            "Select parameter for color mapping", [None, 'DateTime'])
         for index, row in data.iterrows():
             fir_no = row['FIR Number']
             registered_district = row['REGISTERED DISTRICT']
@@ -79,10 +83,10 @@ def app():
             else:
                 color = 'red'
 
-            folium.Marker(location=location, 
+            folium.Marker(location=location,
                           popup=f"FIR Number: {fir_no}<br> Date: {date} <br>Registered District: {registered_district} <br> Crime Type : {crime_type}<br>Registered PS: {registered_ps}",
-                        icon=folium.Icon(color=color)).add_to(map)
-            
+                          icon=folium.Icon(color=color)).add_to(map)
+
         # Create a custom HTML legend
         legend_html = """
         <div style="position: fixed; bottom: 50px; left: 50px; z-index: 1000; background-color: white; padding: 5px; border-radius: 5px; box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);">
@@ -93,17 +97,13 @@ def app():
         map.get_root().html.add_child(folium.Element(legend_html))
 
     elif selected_parameter == 'Heatmap':
-            heat_data = data[['lat', 'lon']]
-            HeatMap(heat_data, radius=15).add_to(map)
+        heat_data = data[['lat', 'lon']]
+        HeatMap(heat_data, radius=15).add_to(map)
 
-            
-            st_folium(map, width=1000, key='11')
-
-        
+        st_folium(map, width=1000, key='11')
 
     st_folium(map, width=1000, key='12')
 
 
 if __name__ == "__main__":
     app()
-
